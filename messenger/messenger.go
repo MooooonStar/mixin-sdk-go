2c8b3a163b0c1dde936d76b6c728b01ceb48d615
snow <ymx@fox.one>
2019-01-06 10:32:51 +0800

    delete listener

--- messenger/messenger.go
+++ messenger/messenger.go
@@ -2,6 +2,8 @@ package messenger
 
 import (
 	"context"
+	"encoding/base64"
+	"encoding/json"
 	"log"
 	"time"
 
@@ -28,5 +30,27 @@ func (m *Messenger) Run(ctx context.Context, listener BlazeListener) {
 			time.Sleep(1 * time.Second)
 		}
 		m.BlazeClient = NewBlazeClient(ClientID, SessionID, SessionKey)
+	}
+}
+
+type DefaultBlazeListener struct{}
+
+// interface to implement if you want to handle the message
+func (l DefaultBlazeListener) OnMessage(ctx context.Context, msg MessageView, userId string) error {
+	log.Println("I got a message: ", msg)
+	data, err := base64.StdEncoding.DecodeString(msg.Data)
+	if err != nil {
+		return err
+	}
+	if msg.Category == "SYSTEM_ACCOUNT_SNAPSHOT" {
+		var transfer TransferView
+		if err := json.Unmarshal(data, &transfer); err != nil {
+			return err
+		}
+		log.Println("I got a snapshot: ", transfer)
+		return nil
+	} else {
+		log.Printf("I got a message, it said: %s", string(data))
+		return nil
 	}
 }
